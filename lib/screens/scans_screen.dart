12abe5dcc4f8f8db9f82281631a74e98e2fc82d6
YuanSamuel <samuel6161@gmail.com>
2020-07-29 19:23:57 -0500

    Add geolocator

--- lib/screens/scans_screen.dart
+++ lib/screens/scans_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ScansScreen extends StatefulWidget {
@@ -10,21 +11,40 @@ class ScansScreen extends StatefulWidget {
 
 class _ScansScreenState extends State<ScansScreen> {
   Completer<GoogleMapController> _controller = Completer();
+  Position _currentLocation;
+
+  @override
+  void initState() {
+    _getCurrentLocation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (_currentLocation == null) {
+      return Scaffold(
+        body: Center(child: CircularProgressIndicator(),),
+      );
+    }
     return Scaffold(
-      body: Scaffold(
         body: GoogleMap(
+          mapType: MapType.hybrid,
           initialCameraPosition: CameraPosition(
-            target: LatLng(37.5, -122.08),
+            target: LatLng(_currentLocation.latitude, _currentLocation.longitude),
             zoom: 14,
           ),
           onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
           },
         ),
-      )
     );
+  }
+
+  _getCurrentLocation() async {
+    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+    Position currentLocation = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    setState(() {
+      _currentLocation = currentLocation;
+    });
   }
 }
