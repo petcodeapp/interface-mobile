a53734c9c25cf9f525ad9f2898e5c533d35d2e75
YuanSamuel <samuel6161@gmail.com>
2020-08-15 13:51:28 -0500

    :sparkles: add session id to address autocomplete provider

--- lib/services/address_autocomplete_provider.dart
+++ lib/services/address_autocomplete_provider.dart
@@ -2,22 +2,23 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:petcode_app/models/Address.dart';
 import 'package:petcode_app/services/address_autocomplete_service.dart';
+import 'package:uuid/uuid.dart';
 
 class AddressAutocompleteProvider extends ChangeNotifier {
-
   AddressAutocompleteService _autocompleteService;
   List<Address> _addresses;
 
-  String _mapsApiKey;
+  List<Address> get addresses => _addresses;
 
   setUpService() async {
-    _autocompleteService = new AddressAutocompleteService();
-    _mapsApiKey = await FlutterSecureStorage().read(key: 'google_maps_key');
+    String mapsApiKey =
+        await FlutterSecureStorage().read(key: 'google_maps_key');
+    _autocompleteService = new AddressAutocompleteService(
+        apiKey: mapsApiKey, sessionToken: Uuid().v4());
   }
 
   void getAddressPredictions(String input) async {
-    _addresses = await _autocompleteService.getLocationResults(input, _mapsApiKey);
+    _addresses = await _autocompleteService.getLocationResults(input);
     notifyListeners();
   }
-
 }
--- lib/services/address_autocomplete_service.dart
+++ lib/services/address_autocomplete_service.dart
@@ -2,15 +2,19 @@ import 'package:dio/dio.dart';
 import 'package:petcode_app/models/Address.dart';
 
 class AddressAutocompleteService {
-  Future<List<Address>> getLocationResults(String input, String apiKey) async {
-    if (input.isNotEmpty) {
+  String apiKey;
+  String sessionToken;
 
-      String baseUrl = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+  AddressAutocompleteService({this.apiKey, this.sessionToken});
+
+  Future<List<Address>> getLocationResults(String input) async {
+    if (input.isNotEmpty) {
+      String baseUrl =
+          'https://maps.googleapis.com/maps/api/place/autocomplete/json';
       String type = 'address';
 
-      //TODO Add session token
-
-      String request = '$baseUrl?input=$input&key=$apiKey&types=$type';
+      String request =
+          '$baseUrl?input=$input&key=$apiKey&types=$type&sessiontoken=$sessionToken';
       Response response = await Dio().get(request);
 
       final predictions = response.data['predictions'];
@@ -19,11 +23,12 @@ class AddressAutocompleteService {
 
       for (int i = 0; i < predictions.length; i++) {
         final structuredFormatting = predictions[i]['structured_formatting'];
-        addresses.add(new Address(mainText: structuredFormatting['main_text'], secondaryText: structuredFormatting['secondary_text']));
+        addresses.add(new Address(
+            mainText: structuredFormatting['main_text'],
+            secondaryText: structuredFormatting['secondary_text']));
       }
 
       return addresses;
     }
   }
-
 }
