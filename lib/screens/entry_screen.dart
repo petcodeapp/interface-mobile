d9d80e0ec105c8ec3af2f99c863641b1080e5cbd
lucaszcai <lucas.z.cai@gmail.com>
2020-09-08 17:50:31 -0500

    added background circles to entry screen

--- lib/screens/entry_screen.dart
+++ lib/screens/entry_screen.dart
@@ -16,65 +16,70 @@ class _EntryScreenState extends State<EntryScreen> {
     double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: StyleConstants.blue,
+      backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: height * 0.075,),
-            Container(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: height,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: 0,
+                left: 0,
+                child: Image.asset(
+                    'assets/images/onboarding/topleftcircles.png',
+                  width:  width,
+                ),
+              ),
+              Positioned(
+                top: 0,
+                left: 0,
+                child: Image.asset(
+                  'assets/images/onboarding/bottomrightcircles.png',
+                  width:  width,
+                ),
+              ),
+              Container(
                 width: width * 0.8,
                 height: height * 0.2,
                 child: Image.asset('assets/images/logoyellow.png', fit: BoxFit.cover,),
-            ),
-            Expanded(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: Image.asset('assets/images/dog1.png'),
-                    ),
-                    Positioned(
-                      bottom: height * 0.3,
-                      child: GestureDetector(
-                        onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => SignupScreen())),
-                        child: Container(
-                          decoration: StyleConstants.roundYellowButtonDeco,
-                          width: 220.0,
-                          height: 60.0,
-                          child: Center(
-                            child: Text('Register',
-                              style: StyleConstants.whiteButtonText,
-                            ),
-                          ),
-                        ),
+              ),
+              Positioned(
+                bottom: height * 0.25,
+                child: GestureDetector(
+                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => LoginScreen())),
+
+                  child: Container(
+                    decoration: StyleConstants.roundYellowButtonDeco,
+                    width: 220.0,
+                    height: 60.0,
+                    child: Center(
+                      child: Text('Login',
+                        style: StyleConstants.whiteTitleText,
                       ),
                     ),
-                    Positioned(
-                      bottom: height * 0.15,
-                      child: GestureDetector(
-                        onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => LoginScreen())),
-                        child: Container(
-                          decoration: StyleConstants.roundWhiteButtonDeco,
-                          width: 220.0,
-                          height: 60.0,
-                          child: Center(
-                            child: Text('Login',
-                              style: StyleConstants.yellowButtonText,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
-            )
-          ],
+              Positioned(
+                bottom: height * 0.15,
+                child: GestureDetector(
+                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => SignupScreen())),
+                  child: Container(
+                    decoration: StyleConstants.roundWhiteButtonDeco.copyWith(color: StyleConstants.blue),
+                    width: 220.0,
+                    height: 60.0,
+                    child: Center(
+                      child: Text('Activate',
+                        style: StyleConstants.whiteTitleText,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
