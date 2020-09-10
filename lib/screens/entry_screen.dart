af024b44f623a5e5febf8f05d39d08b5c16c9e9b
lucaszcai <lucas.z.cai@gmail.com>
2020-09-09 20:04:49 -0500

    set up dots indicator

--- lib/screens/entry_screen.dart
+++ lib/screens/entry_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:petcode_app/screens/login_screen.dart';
 import 'package:petcode_app/screens/signup_screen.dart';
@@ -19,6 +20,8 @@ class _EntryScreenState extends State<EntryScreen> {
     );
     super.initState();
   }
+
+  int pageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -69,119 +72,141 @@ class _EntryScreenState extends State<EntryScreen> {
               Align(
                 alignment: Alignment.center,
                 child: SizedBox(
-                  height: height * 0.35,
-                  child: PageView(
-                    controller: _controller,
-                    scrollDirection: Axis.horizontal,
-                    children: <Widget>[
+                  height: height * 0.38,
+                  width: width,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
                       Container(
-                        child: Center(
-                            child: Column(
+                        height: height * 0.34,
+                        child: PageView(
+                          controller: _controller,
+                          scrollDirection: Axis.horizontal,
+                          onPageChanged: (int page){
+                            setState(() {
+                              pageIndex = page;
+                            });
+                          },
+                          children: <Widget>[
+                            Container(
+                              child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        height: height * 0.25,
+                                          child: Image.asset('assets/images/onboarding/onboarding3.png', fit: BoxFit.cover),
+                                      ),
+                                      Text('One Code', style: TextStyle(
+                                        color: StyleConstants.blue,
+                                        fontSize: 25.0,
+                                        fontWeight: FontWeight.w600,
+                                      )),
+                                      Text('An endless suite of', style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 14.0,
+                                      ),),
+                                      Text('features for pet owners', style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 14.0,
+                                      ),),
+                                    ],
+                                  ),
+                                //child: Text('Text Description of the app 1'),
+                              ),
+                            ),
+                            Container(
+                              child: Center(
+                                //child: Text('Text Description of the app 2'),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      height: height * 0.25,
+                                      child: Image.asset('assets/images/onboarding/onboarding2.png', fit: BoxFit.cover),
+                                    ),
+                                    Text('Peace of Mind', style: TextStyle(
+                                      color: StyleConstants.blue,
+                                      fontSize: 25.0,
+                                      fontWeight: FontWeight.w600,
+                                    )),
+                                    Text('Keep your pet safe and secure', style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 14.0,
+                                    ),),
+                                    Text('with PetCode\'s QR tag system', style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 14.0,
+                                    ),),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Center(
+                                //child: Text('Text Description of the app 3'),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      height: height * 0.25,
+                                      child: Image.asset('assets/images/onboarding/onboarding1.png', fit: BoxFit.cover),
+                                    ),
+                                    Text('Stay Up to Date', style: TextStyle(
+                                      color: StyleConstants.blue,
+                                      fontSize: 25.0,
+                                      fontWeight: FontWeight.w600,
+                                    )),
+                                    Text('PetCode\'s storage and reminder system', style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 14.0,
+                                    ),),
+                                    Text('keeps you updated with your pet\'s health', style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 14.0,
+                                    ),),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
                                 Container(
                                   height: height * 0.25,
-                                    child: Image.asset('assets/images/onboarding/onboarding3.png', fit: BoxFit.cover),
+                                  child: Image.asset('assets/images/onboarding/onboarding4.png', fit: BoxFit.cover),
                                 ),
-                                Text('One Code', style: TextStyle(
+                                Text('Discover Fun', style: TextStyle(
                                   color: StyleConstants.blue,
                                   fontSize: 25.0,
                                   fontWeight: FontWeight.w600,
                                 )),
-                                Text('An endless suite of', style: TextStyle(
+                                Text('Explore nearby pet parks, events, and pet', style: TextStyle(
                                   fontWeight: FontWeight.w500,
                                   fontSize: 14.0,
                                 ),),
-                                Text('features for pet owners', style: TextStyle(
+                                Text('perks with PetCode\'s discover system', style: TextStyle(
                                   fontWeight: FontWeight.w500,
                                   fontSize: 14.0,
                                 ),),
                               ],
                             ),
-                          //child: Text('Text Description of the app 1'),
+                            ),
+                          ],
                         ),
                       ),
-                      Container(
-                        child: Center(
-                          //child: Text('Text Description of the app 2'),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Container(
-                                height: height * 0.25,
-                                child: Image.asset('assets/images/onboarding/onboarding2.png', fit: BoxFit.cover),
-                              ),
-                              Text('Peace of Mind', style: TextStyle(
-                                color: StyleConstants.blue,
-                                fontSize: 25.0,
-                                fontWeight: FontWeight.w600,
-                              )),
-                              Text('Keep your pet safe and secure', style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 14.0,
-                              ),),
-                              Text('with PetCode\'s QR tag system', style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 14.0,
-                              ),),
-                            ],
-                          ),
+                      DotsIndicator(
+                        dotsCount: 4,
+                        decorator: DotsDecorator(
+                          size: Size.square(7.0),
+                          activeSize: Size.square(9.0),
                         ),
-                      ),
-                      Container(
-                        child: Center(
-                          //child: Text('Text Description of the app 3'),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Container(
-                                height: height * 0.25,
-                                child: Image.asset('assets/images/onboarding/onboarding1.png', fit: BoxFit.cover),
-                              ),
-                              Text('Stay Up to Date', style: TextStyle(
-                                color: StyleConstants.blue,
-                                fontSize: 25.0,
-                                fontWeight: FontWeight.w600,
-                              )),
-                              Text('PetCode\'s storage and reminder system', style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 14.0,
-                              ),),
-                              Text('keeps you updated with your pet\'s health', style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 14.0,
-                              ),),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: height * 0.25,
-                            child: Image.asset('assets/images/onboarding/onboarding4.png', fit: BoxFit.cover),
-                          ),
-                          Text('Discover Fun', style: TextStyle(
-                            color: StyleConstants.blue,
-                            fontSize: 25.0,
-                            fontWeight: FontWeight.w600,
-                          )),
-                          Text('Explore nearby pet parks, events, and pet', style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 14.0,
-                          ),),
-                          Text('perks with PetCode\'s discover system', style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 14.0,
-                          ),),
-                        ],
-                      ),
+                        position: 0.0 + pageIndex,
                       ),
                     ],
                   ),
